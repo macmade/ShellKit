03887d5ad1698ef09cdf882db24feb5b586b3320
macmade <macmade@xs-labs.com>
2017-05-11 20:53:59 +0200

    Added a header for types.

--- ShellKit/Classes/ShellKit/SKTypes.h
+++ ShellKit/Classes/ShellKit/SKTypes.h
@@ -23,15 +23,14 @@
  ******************************************************************************/
 
 /*!
- * @file        ShellKit.h
+ * @header      SKTypes.h
  * @copyright   (c) 2017, Jean-David Gadina - www.xs-labs.com
  */
 
-#import <ShellKit/SKTypes.h>
-#import <ShellKit/NSString+ShellKit.h>
-#import <ShellKit/SKObject.h>
-#import <ShellKit/SKRunableObject.h>
-#import <ShellKit/SKShell.h>
-#import <ShellKit/SKTask.h>
-#import <ShellKit/SKTaskGroup.h>
-#import <ShellKit/SKAction.h>
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+
+
+NS_ASSUME_NONNULL_END
